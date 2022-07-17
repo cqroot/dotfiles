bec2f5f0646a2f01aaba7d58a395a2f7a23e3232
cqroot <cqroot@outlook.com>
2022-07-17 12:04:01 +0800

    feat: add bash PS1 config

--- bash/main.sh
+++ bash/main.sh
@@ -7,3 +7,5 @@ source ${SCRIPT_DIR}/aliases.sh
 # fzf
 source /usr/share/fzf/key-bindings.bash
 source /usr/share/fzf/completion.bash
+
+PS1='\n\[\033[01;32m\]➜ \[\033[01;37m\] \W\[\033[01;32m\] »\[\033[00m\] '
