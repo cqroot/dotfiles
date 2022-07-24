1d57e838d3fc0de4abdc8adb1680528b7424d6b2
cqroot <cqroot@outlook.com>
2022-07-24 14:40:33 +0800

    feat: bash prompt add git branch name

--- bash/main.sh
+++ bash/main.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 source ${SCRIPT_DIR}/aliases.sh
+source ${SCRIPT_DIR}/scripts/git-prompt.sh
 
 # fzf
 source /usr/share/fzf/key-bindings.bash
 source /usr/share/fzf/completion.bash
 
-PS1='\n\[\033[01;32m\]➜ \[\033[01;37m\] \W\[\033[01;32m\] »\[\033[00m\] '
+# Go
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOPATH/bin
+
+PS1='\n\[\033[01;32m\]➜ \[\033[01;37m\] \W\[\033[01;32m\]$(__git_ps1) »\[\033[00m\] '
