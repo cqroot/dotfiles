31a11b45a652ac816597aabbddb4125795d1c559
cqroot <cqroot@outlook.com>
2022-09-05 15:22:30 +0800

    fix: polybar load error

--- gui-apps/i3/polybar/launch.sh
+++ gui-apps/i3/polybar/launch.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
+script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+args=(-c "${script_dir}/config.ini" --reload)
+
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if [ "$(xrandr | grep -c "connected")" -gt 1 ]; then
     for m in $(polybar --list-monitors | cut -d":" -f1); do
         if [ "$m" == "HDMI-1" ]; then
-            MAINMON=$m polybar --reload main &
+            MAINMON=$m POLYBAR_DIR=${script_dir} polybar "${args[@]}" main &
         else
-            NOTRAYMON=$m polybar --reload notray &
+            NOTRAYMON=$m POLYBAR_DIR=${script_dir} polybar "${args[@]}" notray &
         fi
     done
 else
-    polybar main 2>&1 | tee -a /tmp/polybar.log &
+    POLYBAR_DIR=${script_dir} polybar "${args[@]}" main 2>&1 | tee -a /tmp/polybar.log &
 fi
 disown
