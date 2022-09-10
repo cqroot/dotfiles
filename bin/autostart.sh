15b2b17cb26078bc91d31b89c94938a3e9a76bc4
cqroot <cqroot@outlook.com>
2022-09-10 20:59:06 +0800

    feat: remove de apps

--- bin/autostart.sh
+++ bin/autostart.sh
@@ -1,17 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-pgrep -x sxhkd >/dev/null || sxhkd &
-feh --bg-fill /home/cqroot/Pictures/wallpaper &
+feh --bg-fill "$HOME/Pictures/wallpaper" &
 
-pgrep -x dunst >/dev/null || dunst &
-pgrep -x xautolock >/dev/null ||
-    xautolock -time 30 -corners 00+- -cornerdelay 1 -locker "sh ~/.bin/lock.sh" &
 pgrep -x fcitx >/dev/null || fcitx &
 pgrep -x nm-applet >/dev/null || nm-applet &
-
 pgrep -x cfw >/dev/null || cfw &
 pgrep -x nutstore >/dev/null || nutstore &
-pgrep -x telegram-desktop >/dev/null || telegram-desktop &
-pgrep -x mailspring >/dev/null || mailspring &
-pgrep -x cherrytree >/dev/null || cherrytree &
+pgrep -x telegram-desktop >/dev/null || telegram-desktop -startintray &
+pgrep -x mailspring >/dev/null || mailspring --background &
+pgrep -x joplin-desktop >/dev/null || joplin-desktop &
 pgrep -x flameshot >/dev/null || flameshot &
