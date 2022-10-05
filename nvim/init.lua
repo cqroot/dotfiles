15fe88112bcaf55925d6748ee4dceb574afeada9
cqroot <cqroot@outlook.com>
2022-10-06 03:04:24 +0800

    Format

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,10 @@
 local utils = require("utils")
 
-utils.load_modules(
-    {
-        "core",
-        "plugins"
-    }
-)
+utils.load_modules({
+	"core",
+	"plugins",
+})
 
 vim.cmd("colorscheme " .. "dracula")
 vim.cmd("exec 'source '.stdpath('config').'/coc.vim'")
+vim.cmd("command W :execute ':silent w !sudo tee % > /dev/null' | :edit!")
