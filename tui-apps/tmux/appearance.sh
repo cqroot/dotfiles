6665067fcfe3be5ba4a242965d2ddf1e48e43b06
cqroot <cqroot@outlook.com>
2022-10-25 03:56:55 +0800

    Change tmux appearance

--- tui-apps/tmux/appearance.sh
+++ tui-apps/tmux/appearance.sh
@@ -1,24 +1,32 @@
 #!/usr/bin/env bash
 
 # *********************************************************
-# Color ***************************************************
+# Options                                                 *
 # *********************************************************
+SESSION_POS=left
+# SESSION_POS=right
 
-# main
-fg_main=#282A2E
-bg_main=default
+tmux set-option -gq status-justify left
+# tmux set-option -gq status-justify centre
 
-# window
-fg_window=$fg_main
-bg_window=$bg_main
+# *********************************************************
+# Colors                                                  *
+# *********************************************************
+MAIN_COLOR=#BD93F9
 
-# window active
-fg_window_active=#EAEAEA
-bg_window_active=$fg_main
+FG_MAIN=#EAEAEA
+BG_MAIN=#282A2E
 
-# session
-fg_session=$fg_main
-bg_session=$bg_main
+FG_PREFIX=$BG_MAIN
+BG_PREFIX=$MAIN_COLOR
+FG_SESSION=$BG_MAIN
+BG_SESSION=$MAIN_COLOR
+
+FG_WINDOW=$FG_MAIN
+BG_WINDOW=$BG_MAIN
+
+FG_WINDOW_ACTIVE=$MAIN_COLOR
+BG_WINDOW_ACTIVE=$BG_MAIN
 
 # *********************************************************
 # Status options ******************************************
@@ -27,43 +35,51 @@ tmux set-option -gq status-interval 1
 tmux set-option -gq status on
 
 # *********************************************************
-# Basic status bar colors *********************************
+# Basic status bar colors                                 *
 # *********************************************************
-tmux set-option -gq status-fg "$fg_main"
-tmux set-option -gq status-bg "$bg_main"
+tmux set-option -gq status-fg "$FG_MAIN"
+tmux set-option -gq status-bg "$BG_MAIN"
 tmux set-option -gq status-attr none
 
 # *********************************************************
 # Modules *************************************************
 # *********************************************************
-session_module="#[fg=$fg_session,bg=$bg_session,bold]  #S "
-prefix_module="#{?client_prefix,~, }"
+session_module="#[fg=$FG_SESSION,bg=$BG_SESSION,bold]  #S "
+prefix_module="#[fg=$FG_PREFIX,bg=$BG_PREFIX]#{?client_prefix,~, }"
+
+if [ "$SESSION_POS" = "left" ]; then
+    tmux set-option -gq status-left "$session_module$prefix_module#[fg=$FG_MAIN,bg=$BG_MAIN] "
+    tmux set-option -gq status-right ''
+else
+    tmux set-option -gq status-left ''
+    tmux set-option -gq status-right "$prefix_module $session_module"
+fi
 
 # *********************************************************
-# Status left *********************************************
+# Status left                                             *
 # *********************************************************
-tmux set-option -gq status-left-fg "$fg_main"
-tmux set-option -gq status-left-bg "$bg_main"
-
-tmux set-option -gq status-left ''
-
-# *********************************************************
-# Status right ********************************************
-# *********************************************************
-tmux set-option -gq status-right-fg "$fg_main"
-tmux set-option -gq status-right-bg "$bg_main"
-tmux set-option -gq status-right "$prefix_module $session_module"
+tmux set-option -gq status-left-fg "$FG_MAIN"
+tmux set-option -gq status-left-bg "$BG_MAIN"
+tmux set-option -gq status-left-length 150
 
 # *********************************************************
-# Window **************************************************
+# Status right                                            *
 # *********************************************************
-tmux set-option -gq status-justify left
+tmux set-option -gq status-right-fg "$FG_MAIN"
+tmux set-option -gq status-right-bg "$BG_MAIN"
 
+# *********************************************************
+# Window                                                  *
+# *********************************************************
 tmux set-option -gq window-status-format ' #I #W'
 tmux set-option -gq window-status-current-format ' #I #W'
 
-tmux set-option -gq window-status-format "#[fg=$fg_window,bg=$bg_window,bold] #I:#W#F "
-tmux set-option -gq window-status-current-format "#[fg=$fg_window_active,bg=$bg_window_active,bold]    #I:#W#F  "
+# tmux set-option -gq window-status-format "#[fg=$FG_WINDOW,bg=$BG_WINDOW,bold] ﱢ #I:#W#F "
+# tmux set-option -gq window-status-current-format "#[fg=$FG_WINDOW_ACTIVE,bg=$BG_WINDOW_ACTIVE,bold] ﱢ #I:#W#F  "
+tmux set-option -gq window-status-format "#[fg=$FG_WINDOW,bg=$BG_WINDOW,bold] #I:#W#F "
+tmux set-option -gq window-status-current-format "#[fg=$FG_WINDOW_ACTIVE,bg=$BG_WINDOW_ACTIVE,bold] #I:#W#F  "
 
-# Window separator
-tmux set-option -gq window-status-separator ""
+# *********************************************************
+# Others                                                  *
+# *********************************************************
+tmux set-option -gq mode-style "bg=$MAIN_COLOR,fg=$BG_MAIN"
