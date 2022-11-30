ff022731eec01cde9aeb79814afcf2596be92c8c
cqroot <cqroot@outlook.com>
2022-11-30 14:55:42 +0800

    refactor(scripts): install-packages.sh

--- scripts/02-dwm.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-git clone https://github.com/cqroot/dwm || return
-cd dwm || return
-make clean install
-cd ..
--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -1,14 +1,81 @@
 #!/usr/bin/env bash
 
+packages=(
+	"yay"
+	"strace" "net-tools"
+	"base-devel" "nodejs" "npm" "go"
+	"clash"
+	"neofetch"
+	"neovim" "nvim-packer-git" "python-pynvim" "ctags"
+	"ripgrep" "lf" "fzf" "htop" "glow" "tmux"
+	"shfmt" "shellcheck" "python-black" "prettier"
+)
+
+desktop_packages=(
+	"xorg-server" "tk" "xclip" "pavucontrol" "xautolock" "xsecurelock"
+	"lxappearance" "dunst" "network-manager-applet" "picom"
+	"flameshot" "maim"
+	"alacritty"
+	"nemo" "nemo-fileroller"
+	"baobab"
+	"firefox" "mailspring" "nutstore"
+
+	# Appearance
+	"arc-gtk-theme" "bibata-cursor-theme"
+
+	# Fonts
+	"ttf-cascadia-code" "nerd-fonts-cascadia-code"
+	"noto-fonts-emoji"
+	"adobe-source-han-sans-cn-fonts" "wqy-microhei"
+
+	# Text editor
+	"xed"
+
+	# Development
+	"meld"
+
+	# Communication
+	"telegram-desktop"
+
+	# Image viewer
+	"feh" "nomacs" "xviewer"
+
+	# Multimedia
+	"mpv" "vlc"
+
+	# Note-taking
+	"obsidian"
+	# "notion-app-enhanced"
+
+	# Rofi
+	"rofi"
+)
+
+yay_packages=(
+	"sublime-text-4"
+	"clash-for-windows-bin"
+	"baidunetdisk-electron"
+	"typora-free"
+)
+
 main() {
-    SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+	# for package in "${packages[@]}" "${desktop_packages[@]}"; do
+	#     pacman -S \
+	#         --noconfirm \
+	#         --noprogressbar \
+	#         --needed \
+	#         --disable-download-timeout \
+	#         "$package"
+	# done
 
-    "${SCRIPT_DIR}/00-tui.sh"
-
-    if [ "$1" = "dwm" ]; then
-        "${SCRIPT_DIR}/01-gui.sh"
-        "${SCRIPT_DIR}/02-dwm.sh"
-    fi
+	for package in "${yay_packages[@]}"; do
+		yay -S \
+			--noconfirm \
+			--noprogressbar \
+			--needed \
+			--disable-download-timeout \
+			"$package"
+	done
 }
 
 main "$@"
