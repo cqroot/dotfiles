eb31bf02caf4ef878fd1b102304e789b011038b0
cqroot <cqroot@outlook.com>
2023-01-05 04:42:57 +0800

    fix: show systray in polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -9,7 +9,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if [ "$(xrandr | grep -c "connected")" -gt 1 ]; then
 	for m in $(polybar --list-monitors | cut -d":" -f1); do
-		if [ "$m" == "HDMI-1" ]; then
+		if [[ "$m" == "HDMI"* ]]; then
 			MAINMON=$m POLYBAR_DIR=${script_path} polybar "${args[@]}" main 2>&1 &
 		else
 			NOTRAYMON=$m POLYBAR_DIR=${script_path} polybar "${args[@]}" notray 2>&1 &
