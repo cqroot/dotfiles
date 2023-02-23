5ee38a945b77b9b67f342087384e56da2f904e9f
cqroot <cqroot@outlook.com>
2023-02-23 22:08:31 +0800

    feat(nvim): auto install lazy.nvim

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -3,16 +3,35 @@ vim.g.python_host_prog = "/usr/bin/python"
 vim.g.python3_host_skip_check = 1
 vim.g.python3_host_prog = "/usr/bin/python3"
 
-for pkg, _ in pairs(package.loaded) do
-    if pkg:find("core.", 1, true) == 1
-        or pkg:find("plugins.", 1, true) == 1
-        or pkg:find("custom_", 1, true)
-        or pkg == "cheevim"
-    then
-        package.loaded[pkg] = nil
-    end
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
+vim.opt.rtp:prepend(lazypath)
 
-require("cheevim").init()
+-- for pkg, _ in pairs(package.loaded) do
+--     if pkg:find("core.", 1, true) == 1
+--         or pkg:find("plugins.", 1, true) == 1
+--         or pkg:find("custom_", 1, true)
+--         or pkg == "cheevim"
+--     then
+--         package.loaded[pkg] = nil
+--     end
+-- end
+
+require("core.setup").load()
+
+require("plugins").load()
+require("plugins.cmp").load()
+
+require("core.mappings").load()
+require("core.after_plugins").load()
 
 vim.cmd("command! SudoWrite :execute ':silent w !sudo tee % > /dev/null' | :edit!")
