4562c490cb505c02cf5793f95497773e70f4084c
cqroot <cqroot@outlook.com>
2023-02-23 17:50:58 +0800

    feat: update autostart

--- bin/autostart.sh
+++ bin/autostart.sh
@@ -19,10 +19,10 @@ pgrep -x nm-applet >/dev/null || nm-applet &
 pgrep -x cfw >/dev/null || cfw &
 pgrep -x nutstore >/dev/null || nutstore &
 pgrep -x telegram-desktop >/dev/null || telegram-desktop -startintray &
-# pgrep -x mailspring >/dev/null || mailspring --background &
+pgrep -x mailspring >/dev/null || mailspring --background &
 pgrep -x joplin-desktop >/dev/null || joplin-desktop &
 pgrep -x flameshot >/dev/null || flameshot &
 # pgrep -x albert >/dev/null || albert &
-pgrep -x firefox >/dev/null || firefox &
+# pgrep -x firefox >/dev/null || firefox &
 
 # setxkbmap -layout us -option ctrl:swapcaps
