f1c7a63913714e18e95224a9820dc44787e05ced
cqroot <cqroot@outlook.com>
2023-02-27 13:42:18 +0800

    feat: use different bash configurations for different platforms

--- home/dot_config/bash/main.bash
+++ home/dot_config/bash/main.bash
@@ -1,21 +1,33 @@
 #!/usr/bin/env bash
 
-SCRIPT_PATH=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 # $HOME/.config -> /home/user/.config
 shopt -u direxpand
 # $HOME/.config -> \$HOME/.config
 shopt -u progcomp
 
-source "${SCRIPT_PATH}/envs.bash"
-source "${SCRIPT_PATH}/aliases.bash"
-source "${SCRIPT_PATH}/aliases_typo.bash"
-source "${SCRIPT_PATH}/zoxide.bash"
+source "${SCRIPT_DIR}/envs.bash"
+source "${SCRIPT_DIR}/aliases.bash"
+source "${SCRIPT_DIR}/aliases_typo.bash"
 
-# fzf
-source "/usr/share/fzf/key-bindings.bash"
-source "/usr/share/fzf/completion.bash"
+PS1='\[\033[01;32m\]➜ \[\033[01;37m\] \W\[\033[01;32m\]$(__git_ps1) »\[\033[00m\] '
 
-# PS1='\[\033[01;32m\]➜ \[\033[01;37m\] \W\[\033[01;32m\]$(__git_ps1) »\[\033[00m\] '
+case $(uname -s) in
+Linux*)
+	eval "$(starship init bash)"
 
-eval "$(starship init bash)"
+	source "${SCRIPT_DIR}/zoxide.bash"
+
+	# fzf
+	source "/usr/share/fzf/key-bindings.bash"
+	source "/usr/share/fzf/completion.bash"
+	;;
+Darwin*) ;;
+CYGWIN*) ;;
+MINGW*)
+	source "${SCRIPT_DIR}/fzf/key-bindings.bash"
+	source "${SCRIPT_DIR}/fzf/completion.bash"
+	;;
+*) ;;
+esac
