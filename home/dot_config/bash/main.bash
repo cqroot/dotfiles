204ca3b9376cc9f2db57f43a473ef67b1f524930
cqroot <cqroot@outlook.com>
2023-03-01 12:14:58 +0800

    feat: remove zoxide and starship

--- home/dot_config/bash/main.bash
+++ home/dot_config/bash/main.bash
@@ -13,13 +13,12 @@ source "${SCRIPT_DIR}/aliases_typo.bash"
 
 PS1='\[\033[01;32m\]➜ \[\033[01;37m\] \W\[\033[01;32m\]$(__git_ps1) »\[\033[00m\] '
 
+# source "${SCRIPT_DIR}/zoxide.bash"
+# eval "$(starship init bash)"
+
+# fzf
 case $(uname -s) in
 Linux*)
-	eval "$(starship init bash)"
-
-	source "${SCRIPT_DIR}/zoxide.bash"
-
-	# fzf
 	source "/usr/share/fzf/key-bindings.bash"
 	source "/usr/share/fzf/completion.bash"
 	;;
