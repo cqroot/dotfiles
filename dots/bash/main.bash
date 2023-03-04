d82d5240a81e3472fc75dbb28fb5152056fccfdb
cqroot <cqroot@outlook.com>
2023-03-04 21:28:08 +0800

    feat: use gmdots to manage bash config

--- dots/bash/main.bash
+++ dots/bash/main.bash
@@ -13,26 +13,21 @@ source "${SCRIPT_DIR}/aliases_typo.bash"
 
 PS1='\[\033[01;32m\]➜ \[\033[01;37m\] \W\[\033[01;32m\]$(__git_ps1) »\[\033[00m\] '
 
-# source "${SCRIPT_DIR}/zoxide.bash"
-# eval "$(starship init bash)"
+case $(uname -s) in
+Linux*)
+	eval "$(starship init bash)"
 
-# fzf
-if [ -f "/usr/share/fzf/key-bindings.bash" ]; then
+	source "${SCRIPT_DIR}/zoxide.bash"
+
+	# fzf
 	source "/usr/share/fzf/key-bindings.bash"
-else
-	source "${SCRIPT_DIR}/fzf/key-bindings.bash"
-fi
-
-if [ -f "/usr/share/fzf/completion.bash" ]; then
 	source "/usr/share/fzf/completion.bash"
-else
+	;;
+Darwin*) ;;
+CYGWIN*) ;;
+MINGW*)
+	source "${SCRIPT_DIR}/fzf/key-bindings.bash"
 	source "${SCRIPT_DIR}/fzf/completion.bash"
-fi
-
-# case $(uname -s) in
-# Linux*) ;;
-# Darwin*) ;;
-# CYGWIN*) ;;
-# MINGW*) ;;
-# *) ;;
-# esac
+	;;
+*) ;;
+esac
