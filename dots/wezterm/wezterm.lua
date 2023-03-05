c4cc685b2e035d1605875829b3bc161738895188
cqroot <cqroot@outlook.com>
2023-03-05 22:44:51 +0800

    feat(wezterm): disable the key binding of Alt+Enter

--- dots/wezterm/wezterm.lua
+++ dots/wezterm/wezterm.lua
@@ -9,13 +9,22 @@ return {
 	font = wezterm.font("CaskaydiaCove Nerd Font"),
 	font_size = 14.0,
 
-	disable_default_key_bindings = true,
-
 	mouse_bindings = {
 		{
 			event = { Up = { streak = 1, button = "Right" } },
 			mods = "NONE",
 			action = wezterm.action.Paste,
+		},
+	},
+
+	-- disable_default_key_bindings = true,
+	keys = {
+		-- Turn off the default CMD-m Hide action, allowing CMD-m to
+		-- be potentially recognized and handled by the tab
+		{
+			key = "Enter",
+			mods = "ALT",
+			action = wezterm.action.DisableDefaultAssignment,
 		},
 	},
 }
