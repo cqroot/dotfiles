ed1f16c54d346c1b011529d5e0ee406038f6f463
cqroot <cqroot@outlook.com>
2023-03-05 19:48:12 +0800

    feat(wezterm): disable default key bindings

--- dots/wezterm/wezterm.lua
+++ dots/wezterm/wezterm.lua
@@ -2,10 +2,15 @@ local wezterm = require("wezterm")
 
 return {
 	default_prog = { "C:/Program Files/Git/bin/bash.exe" },
+
 	-- color_scheme = "Nord",
 	color_scheme = "Dracula+",
+
 	font = wezterm.font("CaskaydiaCove Nerd Font"),
 	font_size = 14.0,
+
+	disable_default_key_bindings = true,
+
 	mouse_bindings = {
 		{
 			event = { Up = { streak = 1, button = "Right" } },
