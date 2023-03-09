766ec77a549706005cffe0ed1f64bf60eb6cfd4b
cqroot <cqroot@outlook.com>
2023-03-09 18:16:15 +0800

    refactor(nvim): remove useless file

--- nvim/lua/default_options.lua
+++
@@ -1,8 +0,0 @@
-local M = {}
-
-M.workbench_ColorScheme = "onedark"
--- M.workbench_ColorScheme = "zenbones"
--- M.workbench_ColorScheme = "ayu-mirage"
--- M.workbench_ColorScheme = "vscode"
-
-return M
