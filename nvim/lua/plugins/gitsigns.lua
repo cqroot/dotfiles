23664e7db72cb5192d972a8d7a9f13bfb7ffe346
Keith Chu <cqroot@outlook.com>
2023-07-23 01:35:52 +0800

    ✨ (nvim): remove nvim config

--- nvim/lua/plugins/gitsigns.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
-function M.load()
-    require("gitsigns").setup({
-        current_line_blame = true,
-    })
-end
-
-return M
--- nvim/lua/plugins/markdown-preview.lua
+++
@@ -1,8 +0,0 @@
-local M = {}
-
-function M.load()
-    vim.g.mkdp_auto_close = 0
-    vim.g.mkdp_browser = ""
-end
-
-return M
