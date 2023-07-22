8a26b94068b3b2b18b90b95e2b5116473a1d51d4
Keith Chu <cqroot@outlook.com>
2023-07-22 22:16:08 +0800

    ✨ (nvim): telescope quit on first single esc

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,5 +1,17 @@
 local M = {}
 
-function M.load() end
+function M.load()
+    local actions = require("telescope.actions")
+
+    require("telescope").setup({
+        defaults = {
+            mappings = {
+                i = {
+                    ["<esc>"] = actions.close,
+                },
+            },
+        },
+    })
+end
 
 return M
