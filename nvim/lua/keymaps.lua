132112442621328e0d70cfcb19fffbdbf796d728
Keith Chu <cqroot@outlook.com>
2025-09-19 21:50:03 +0800

    feat(nvim): add lsp diagnose keymap

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -7,6 +7,14 @@ function ActionFormat()
     })
 end
 
+function ActionLspOpenDiagnostic()
+    vim.diagnostic.open_float({ scope = "line", border = "rounded" })
+end
+
+function ActionLspFormat()
+    vim.lsp.buf.format({ async = true })
+end
+
 function M.setup()
     local opts = { noremap = true, silent = true }
     -- Buffer
@@ -35,39 +43,27 @@ function M.setup()
     vim.keymap.set({ "n", "v", "i" }, "<C-t>", require("telescope.builtin").tags, opts)
 
     require("which-key").add({
-        { mode = { "n", "v" }, "<leader>tg", require("telescope.builtin").live_grep,   desc = "Telescope live_grep" },
-        { mode = { "n", "v" }, "<leader>ts", require("telescope.builtin").grep_string, desc = "Telescope grep_string" },
-        { mode = { "n", "v" }, "<leader>tb", require("telescope.builtin").buffers,     desc = "Telescope buffers" },
-        { mode = { "n", "v" }, "<leader>tr", require("telescope.builtin").registers,   desc = "Telescope registers" },
-        { mode = { "n", "v" }, "<leader>gd", "<CMD>Gitsigns diffthis<CR>",             desc = "Gitsigns diffthis" },
-        { mode = { "n", "v" }, "<leader>lr", vim.lsp.buf.rename,                       desc = "LSP rename" },
-        { mode = "n",          "gD",         vim.lsp.buf.declaration,                  desc = "LSP declaration" },
-        { mode = "n",          "gd",         vim.lsp.buf.definition,                   desc = "LSP definition" },
-        { mode = "n",          "H",          vim.lsp.buf.hover,                        desc = "LSP hover" },
-        { mode = "n",          "gi",         vim.lsp.buf.implementation,               desc = "LSP inplementation" },
-        { mode = "n",          "<C-k>",      vim.lsp.buf.signature_help,               desc = "LSP signature_help" },
-        { mode = "n",          "<leader>wa", vim.lsp.buf.add_workspace_folder,         desc = "LSP add_workspace_folder" },
-        { mode = "n",          "<leader>wr", vim.lsp.buf.remove_workspace_folder,      desc = "LSP remove_workspace_folder" },
-        {
-            mode = "n",
-            "<leader>wl",
-            function()
-                print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-            end,
-            desc = "LSP list_workspace_folders"
-        },
-        { mode = "n",          "<leader>D",  vim.lsp.buf.type_definition, desc = "LSP type_definition" },
-        { mode = "n",          "<leader>lr", vim.lsp.buf.rename,          desc = "LSP rename" },
-        { mode = { "n", "v" }, "<leader>ca", vim.lsp.buf.code_action,     desc = "LSP code_action" },
-        { mode = "n",          "gr",         vim.lsp.buf.references,      desc = "LSP references" },
-        {
-            mode = "n",
-            "<space>f",
-            function()
-                vim.lsp.buf.format({ async = true })
-            end,
-            desc = "LSP format"
-        }
+        { mode = { "n", "v", "i" }, "<M-a>",      "<C-o>",                                  desc = "Jump back" },
+        { mode = { "n", "v", "i" }, "<M-d>",      "<C-i>",                                  desc = "Jump forward" },
+        { mode = { "n", "v" },      "<leader>tg", require("telescope.builtin").live_grep,   desc = "Telescope live_grep" },
+        { mode = { "n", "v" },      "<leader>ts", require("telescope.builtin").grep_string, desc = "Telescope grep_string" },
+        { mode = { "n", "v" },      "<leader>tb", require("telescope.builtin").buffers,     desc = "Telescope buffers" },
+        { mode = { "n", "v" },      "<leader>tr", require("telescope.builtin").registers,   desc = "Telescope registers" },
+        { mode = { "n", "v" },      "<leader>gd", "<CMD>Gitsigns diffthis<CR>",             desc = "Gitsigns diffthis" },
+        { mode = { "n", "v" },      "<leader>lr", vim.lsp.buf.rename,                       desc = "LSP rename" },
+        { mode = "n",               "gD",         vim.lsp.buf.declaration,                  desc = "LSP declaration" },
+        { mode = "n",               "gd",         vim.lsp.buf.definition,                   desc = "LSP definition" },
+        { mode = "n",               "H",          vim.lsp.buf.hover,                        desc = "LSP hover" },
+        { mode = "n",               "gi",         vim.lsp.buf.implementation,               desc = "LSP inplementation" },
+        { mode = "n",               "<C-k>",      vim.lsp.buf.signature_help,               desc = "LSP signature_help" },
+        { mode = "n",               "<leader>wa", vim.lsp.buf.add_workspace_folder,         desc = "LSP add_workspace_folder" },
+        { mode = "n",               "<leader>wr", vim.lsp.buf.remove_workspace_folder,      desc = "LSP remove_workspace_folder" },
+        { mode = "n",               "<leader>D",  vim.lsp.buf.type_definition,              desc = "LSP type_definition" },
+        { mode = "n",               "<leader>lr", vim.lsp.buf.rename,                       desc = "LSP rename" },
+        { mode = { "n", "v" },      "<leader>ca", vim.lsp.buf.code_action,                  desc = "LSP code_action" },
+        { mode = "n",               "gr",         vim.lsp.buf.references,                   desc = "LSP references" },
+        { mode = "n",               "<leader>le", ActionLspOpenDiagnostic,                  desc = "LSP show diagnostics" },
+        { mode = "n",               "<space>f",   ActionLspFormat,                          desc = "LSP format" },
     })
 end
 
