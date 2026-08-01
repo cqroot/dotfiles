34f1c0c4427af52ecdb7dc522d9164327145f580
Keith Chu <cqroot@outlook.com>
2026-08-02 03:39:38 +0800

    feat: initial commit

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,50 @@
+if not status is-interactive
+    exit
+end
+
+set fish_greeting
+
+set -gx EDITOR "nvim"
+set -gx VISUAL "nvim"
+set -gx PAGER "less"
+set -gx MANPAGER "nvim +Man!"
+set -gx PAGER "bat"
+set -gx GOPATH "$HOME/go"
+
+fish_add_path $HOME/.bin
+fish_add_path $HOME/.secret_bin
+fish_add_path $HOME/.local/bin
+fish_add_path $HOME/.cargo/bin
+if set -q GOPATH
+    fish_add_path $GOPATH/bin
+end
+
+abbr srcfish 'source ~/.config/fish/config.fish'
+abbr ls 'ls --color=auto'
+abbr ll 'ls -lh --group-directories-first'
+abbr la 'ls -lha --group-directories-first'
+abbr l. "ls -A | grep -E '^\.'"
+abbr grep 'grep --color=auto'
+abbr gs 'git status'
+abbr gll "git log --pretty=format:'%C(yellow)%h %Cgreen%ad %Creset%s' --date=short"
+abbr gl 'gll -n 20'
+abbr gd 'git diff'
+abbr gdc 'git diff --cached'
+abbr paru 'env all_proxy=http://127.0.0.1:7890 paru'
+abbr srctmux 'tmux source-file ~/.config/tmux/tmux.conf'
+abbr tl 'tmux ls'
+abbr tn 'tmux new -s (basename $PWD)'
+abbr ta 'tmux attach -t'
+abbr nvi nvim
+abbr lg lazygit
+abbr oc opencode
+abbr cz chezmoi
+function czcd
+    cd (chezmoi source-path $argv)
+end
+
+set -gx FZF_DEFAULT_OPTS '--cycle --border=rounded --pointer=• --prompt="❯ "'
+fzf --fish | source
+
+zoxide init --cmd cd fish | source
+starship init fish | source
