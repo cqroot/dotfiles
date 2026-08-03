f059795c685d1d6124e127e84ee735b93b116211
Keith Chu <cqroot@outlook.com>
2026-08-04 00:40:28 +0800

    feat(fedora): update installation script to include additional packages and cleanup

--- scripts/install_for_fedora_linux.bash
+++ scripts/install_for_fedora_linux.bash
@@ -15,8 +15,8 @@ sudo dnf copr enable -y dejan/lazygit
 sudo dnf copr enable -y atim/starship
 sudo dnf install -y \
     bat \
+    cascadia-code-nf-fonts \
     chezmoi \
-    copyq \
     fish \
     fzf \
     ghostty \
@@ -24,14 +24,33 @@ sudo dnf install -y \
     git-delta \
     go \
     lazygit \
-    mpv \
+    neovim \
+    papirus-icon-theme \
     python3-pip \
     ripgrep \
     sqlite \
     starship \
-    thunderbird \
     vim \
     zoxide
 
+sudo dnf remove -y \
+    gnome-connections \
+    gnome-contacts \
+    gnome-maps \
+    gnome-tour
+
+sudo dnf autoremove -y
+
+sudo flatpak remote-modify --disable fedora
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 sudo flatpak remote-modify flathub --url=https://mirrors.ustc.edu.cn/flathub
 # sudo flatpak remote-modify flathub --url=https://mirrors.sjtug.sjtu.edu.cn/flathub
+
+flatpak install -y flathub com.brave.Browser
+flatpak install -y flathub com.github.hluk.copyq
+flatpak install -y flathub com.mattjakeman.ExtensionManager
+flatpak install -y flathub com.visualstudio.code
+flatpak install -y flathub io.mpv.Mpv
+flatpak install -y flathub md.obsidian.Obsidian
+flatpak install -y flathub org.flameshot.Flameshot
+flatpak install -y flathub org.mozilla.thunderbird
